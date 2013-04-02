357bd7294bbacb2c950f18125afc5d24c67ee0b9
Shrek Zhou <zgwmike@hotmail.com>
2013-04-03 07:30:46 +0800

    rest services

--- webservice/src/main/java/com/updis/rest/MessageResource.java
+++ webservice/src/main/java/com/updis/rest/MessageResource.java
@@ -1,8 +1,17 @@
 package com.updis.rest;
 
 import com.updis.entity.Message;
+import com.updis.erpclient.config.ERPConfig;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.ws.rs.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,12 +20,15 @@ import javax.ws.rs.*;
  * Time: 9:40 PM
  * To change this template use File | Settings | File Templates.
  */
-@Path("/{uuid}/messages")
+@Controller
+@RequestMapping("/messages")
 public class MessageResource {
-    @GET
-    @Produces("application/json")
-    @Path("/{categoryName}")
-    public Message getMessages(@PathParam("uuid") String uuid, @PathParam("categoryName") String categoryName) {
-        return new Message(uuid, categoryName);
+    private ERPConfig erpConfig;
+
+    @RequestMapping("/fetchListData")
+    @ResponseBody
+    public List<Message> fetchMessageList(@RequestParam("uuid") String uuid, @RequestParam("categoryType") Integer categoryType, @RequestParam(value = "currentPage", defaultValue = "1") Integer currentPage) {
+
+        return Arrays.asList(new Message[]{new Message("Title", "Content")});
     }
 }

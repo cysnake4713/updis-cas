0df545227f6757ad0e2205be8cc4c1035b944a59
cysnake4713 <cysnake4713@gmail.com>
2013-04-27 17:08:22 +0800

    UPDIS-63 fix bug!! add config

---
+++ erpclient/src/main/java/com/updis/erpclient/config/ServiceConfig.java
@@ -0,0 +1,21 @@
+package com.updis.erpclient.config;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cysnake4713
+ * Date: 13-4-26
+ * Time: 下午4:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServiceConfig {
+    private String urlAddress;
+
+    public String getUrlAddress() {
+        return urlAddress;
+    }
+
+    public void setUrlAddress(String urlAddress) {
+        this.urlAddress = urlAddress;
+    }
+
+}

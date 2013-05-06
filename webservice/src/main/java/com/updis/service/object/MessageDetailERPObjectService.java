775830b77ee7b6f9d8d76087bf6d9aef9281b814
HaiLee <comcuter@gmail.com>
2013-05-07 00:03:08 +0800

    1.修正时间显示的问题. 2. 没有符合某个时间段的消息时,会返回一大堆信息,进行过滤.

--- webservice/src/main/java/com/updis/service/object/MessageDetailERPObjectService.java
+++ webservice/src/main/java/com/updis/service/object/MessageDetailERPObjectService.java
@@ -3,12 +3,16 @@ package com.updis.service.object;
 import com.updis.entity.MessageDetail;
 import com.updis.erpclient.criteria.Criteria;
 import com.updis.service.converter.ERPObjectConvertService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -21,6 +25,7 @@ import java.util.List;
 @Service("messageDetailService")
 public class MessageDetailERPObjectService extends AbstractERPObjectService {
     private static String MODEL_NAME = "message.message";
+    private Logger logger = LoggerFactory.getLogger(MessageDetailERPObjectService.class);
 
     @Autowired
     private ERPObjectConvertService messageDetailConverter;
@@ -36,20 +41,37 @@ public class MessageDetailERPObjectService extends AbstractERPObjectService {
     }
 
     public List<MessageDetail> getMesagesBetweenDate(Date fromDate, Date toDate) {
-//        List<MessageDetail> messageDetails;
-//
-//        List<Criteria> criterias = new ArrayList<Criteria>();
-//        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
-//        criterias.add(new Criteria("write_date", ">", formatter.format(fromDate)));
-//        criterias.add(new Criteria("write_date", "<", formatter.format(toDate)));
-//        messageDetails = find(criterias, null, null, (String)null);
+        List<MessageDetail> messageDetails;
 
-        List<MessageDetail> messageDetails = new ArrayList<MessageDetail>();
-        MessageDetail m = new MessageDetail();
-        m.setContentId(10135);
-        m.setTitle("测试用标题,这是个好人");
-        messageDetails.add(m);
+        List<Criteria> criterias = new ArrayList<Criteria>();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+        criterias.add(new Criteria("create_date_display", ">", formatter.format(fromDate)));
+        criterias.add(new Criteria("create_date_display", "<", formatter.format(toDate)));
+        messageDetails = find(criterias, null, null, "name", "id", "create_date_display");
+
+        // 有时候ERP会抽风返回一大堆数据,具体原因不明,只能暂时这样做一下过滤.
+        if (messageDetails != null) {
+            SimpleDateFormat createDateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            for (Iterator<MessageDetail> iter = messageDetails.iterator(); iter.hasNext();) {
+                MessageDetail message = iter.next();
+                try {
+                    Date messageCreateDate = createDateFormatter.parse(message.getDatetime());
+                    if (messageCreateDate.getTime() < fromDate.getTime() ||
+                            messageCreateDate.getTime() > toDate.getTime()) {
+                        iter.remove();
+                    }
+                } catch (ParseException e) {
+                    // ignore the error;
+                    logger.debug("the create date format of message is not correct");
+                }
+            }
+        }
+
+//        List<MessageDetail> messageDetails = new ArrayList<MessageDetail>();
+//        MessageDetail m = new MessageDetail();
+//        m.setContentId(10135);
+//        m.setTitle("测试用标题,这是个好人");
+//        messageDetails.add(m);
         return messageDetails;
     }
-
 }

7ffb716613959483191675922e93c1dfb9314c6b
HaiLee <comcuter@gmail.com>
2013-05-12 02:11:54 +0800

    优化代码.

--- erpclient/src/main/java/com/updis/erpclient/ObjectServiceImpl.java
+++ erpclient/src/main/java/com/updis/erpclient/ObjectServiceImpl.java
@@ -34,6 +34,36 @@ public class ObjectServiceImpl extends ServiceBase implements ObjectService {
     }
 
     @Override
+    public List<Integer> search(ERPConfig config, List<Criteria> domain, Integer offset, Integer limit, String order, Map context, boolean count) throws Exception {
+        Object[] ids = (Object[]) this.execute(config, "search", criteriaService.toDomains(domain), offset, limit, order, context, count);
+        List<Integer> ret = new ArrayList<Integer>(ids.length);
+        for (Object id : ids) {
+            ret.add((Integer) id);
+        }
+        return ret;
+    }
+
+    @Override
+    public List<Integer> search(ERPConfig config, List<Criteria> domain) throws Exception {
+        return this.search(config, domain, 0, 20, null, null, false);
+    }
+
+    @Override
+    public List<Map<String, Object>> read(ERPConfig config, List<Integer> ids, String... fields) throws Exception {
+        Object[] results = (Object[])execute(config, "read", ids, fields);
+        List<Map<String, Object>> ret = new ArrayList<Map<String, Object>>(results.length);
+        for (Object obj : results) {
+            ret.add((Map<String, Object>) obj);
+        }
+        return ret;
+    }
+
+    @Override
+    public List<Map<String, Object>> read(ERPConfig config, List<Integer> ids) throws Exception {
+        return this.read(config, ids, null);
+    }
+
+    @Override
     public List<Map<String, Object>> searchRead(ERPConfig config, List<Criteria> domain, String... fields) throws Exception {
         return this.read(config, this.search(config, domain), fields);
     }
@@ -49,46 +79,16 @@ public class ObjectServiceImpl extends ServiceBase implements ObjectService {
     }
 
     @Override
-    public List<Integer> search(ERPConfig config, List<Criteria> domain, Integer offset, Integer limit, String order, Map context, boolean count) throws Exception {
-        Object[] ids = (Object[]) this.execute(config, "search", new Object[]{criteriaService.toDomains(domain), offset, limit, order, context, count});
-        List<Integer> ret = new ArrayList<Integer>(ids.length);
-        for (Object id : ids) {
-            ret.add((Integer) id);
-        }
-        return ret;
-    }
-
-    @Override
-    public List<Integer> search(ERPConfig config, List<Criteria> domain) throws Exception {
-        return this.search(config, domain, 0, 20, null, null, false);
-    }
-
-    @Override
-    public List<Map<String, Object>> read(ERPConfig config, List<Integer> ids, String... fields) throws Exception {
-        Object[] results = (Object[]) this.getConnector().send("execute", config.getDb(), config.getUid(), config.getPassword(), config.getModelName(), "read", ids, fields);
-        List<Map<String, Object>> ret = new ArrayList<Map<String, Object>>(results.length);
-        for (Object obj : results) {
-            ret.add((Map<String, Object>) obj);
-        }
-        return ret;
-    }
-
-    @Override
-    public List<Map<String, Object>> read(ERPConfig config, List<Integer> ids) throws Exception {
-        return this.read(config, ids, null);
-    }
-
-    @Override
     public Boolean update(ERPConfig config, List<Integer> ids, Map<String, Object> vals) throws Exception {
-        return (Boolean) this.getConnector().send("execute", config.getDb(), config.getUid(), config.getPassword(), config.getModelName(), "write", ids, vals);
+        return (Boolean)execute(config, "write", ids, vals);
     }
 
     @Override
     public Integer create(ERPConfig config, Map<String, Object> vals) throws Exception {
-        return (Integer) this.getConnector().send("execute", config.getDb(), config.getUid(), config.getPassword(), config.getModelName(), "create", vals);
+        return (Integer)execute(config, "create", vals);
     }
 
-    private Object execute(ERPConfig config, String method, Object[] params) throws Exception {
+    private Object execute(ERPConfig config, String method, Object... params) throws Exception {
         List<Object> p = new ArrayList<Object>();
         p.add(config.getDb());
         p.add(config.getUid());
@@ -100,15 +100,20 @@ public class ObjectServiceImpl extends ServiceBase implements ObjectService {
         try {
             StringBuffer paramsSB = new StringBuffer();
             for(Object param : params) {
-                // criterias
                 if (param instanceof Object[]) {
-                    Object[] criterias = (Object[])param;
-                    for (Object criteria : criterias) {
-                        Object[] criteriaTuple = (Object[])criteria;
-                        paramsSB.append("['").append(criteriaTuple[0].toString()).append("', '")
-                                .append(criteriaTuple[1].toString()).append("', '")
-                                .append(criteriaTuple[2].toString()).append("'] ");
+                    Object[] arrayParam = (Object[])param;
+                    for (Object o : arrayParam) {
+                        if (o != null && (o instanceof Object[])) { // criterias
+                            Object[] criteriaTuple = (Object[])o;
+                            paramsSB.append("['").append(criteriaTuple[0].toString()).append("', '")
+                                    .append(criteriaTuple[1].toString()).append("', '")
+                                    .append(criteriaTuple[2].toString()).append("'] ");
+                        } else {
+                            paramsSB.append(o == null ? "null" : o.toString() + " ");
+                        }
                     }
+                } else if (param != null && (param instanceof List)) { // List<Integer>
+                    paramsSB.append("/*ids for short*/ ");
                 } else {
                     paramsSB.append(param == null ? "null " : param.toString() + " ");
                 }
@@ -117,6 +122,7 @@ public class ObjectServiceImpl extends ServiceBase implements ObjectService {
                     + method + " " + paramsSB.toString());
         }catch (Exception e) {
             // ignore exceptions.
+            logger.warn(e.getMessage(), e);
         }
 
         return this.getConnector().send("execute", p);
@@ -125,5 +131,4 @@ public class ObjectServiceImpl extends ServiceBase implements ObjectService {
     private Object exec_workflow(String signal, Object[] params) throws Exception {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
-
 }
